8a710280d346c0d4116018697621f77f2f3405ed
pdebasis7 <pdebasis2013@gmail.com>
2024-02-21 01:26:10 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Debasis P',
+    author_email= 'pdebasis2013@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
